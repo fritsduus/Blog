39f2984e05cb4acdebff4e265d4638c8c8bb5747
Frits Duus Jorgensen <frits.duus@gmail.com>
2013-04-03 21:24:17 +0200

    The inital site
    Created from the MVC4 Internet template changed the _Layout to a Twitter
    Bootstrap template.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to my new blog.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Who am I ?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
